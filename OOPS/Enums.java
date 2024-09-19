a6ad9b2ac25ffb25916afda1bf059738281cbab3
Abubakker Siddiq <abubakkersiddiq307@gmail.com>
2024-09-19 23:16:54 +0530

    OOPS

---
+++ OOPS/Enums.java
@@ -0,0 +1,10 @@
+package OOPS;
+
+class Enums {
+    public enum Test {
+        sunday, monday, tuesday, wednesday, thursday, friday, saturday
+    }
+    
+    public static void main(String[] args) {
+    }
+}
